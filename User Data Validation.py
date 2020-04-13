2323405b99dde2772c50700c9b7f1915d1b0e404
Emem <61992570+trevelyn487@users.noreply.github.com>
2020-04-13 11:14:34 +0100

    Update User Data Validation.py

--- User Data Validation.py
+++ User Data Validation.py
@@ -4,7 +4,7 @@ import random
 
 is_user_validation = True
 
-#creating "container" to hold details of all users 
+#creating "ultimate container" where all users' details are stored 
 database = {}
 
 #initialising number of users
@@ -12,10 +12,12 @@ user = 1
 
 while True:
 
-    first_name = input("Enter First Name: ")
+    #collecting user details
+    first_name = input("\nEnter First Name: ")
     last_name = input("Enter Last Name: ")
     email = input("Enter Email: ")   
 
+    #creating temporary container for each user
     user_details = {
         "First Name": first_name, 
         "Last Name": last_name, 
@@ -30,23 +32,23 @@ while True:
     gen_password = (first_name[:2] + random_string + last_name[-2:])
 
     #user opinion on generated password check
-    print("Recommended password is - " + gen_password + ''' 
-Would you like to use this as your password? 
+    print("\nRecommended password is - " + gen_password + ''' 
+\nWould you like to use this as your password? 
 If so, respond with a "Yes". If not, respond with a "No".''')
 
-    answer = input("Response: ").lower()
+    answer = input("\nResponse: ").lower()
     if answer == "yes":
         print("Your registration was successful.")
 
-        #adding recommended password to user's details
+        #adding recommended password to user's details if user finds it satisfactory
         if "Password" not in user_details.keys():
             user_details["Password"] = gen_password
             database[user] = user_details
                 
-        
+    #allowing user come up with password   
     else:
         print('''
-You can enter your preferred password below. 
+\nYou can enter your preferred password below. 
 NOTE: Password must be at least 7 characters long.''')
         user_password = input("Enter your preferred password: ")
         while len(user_password) < 7:
@@ -58,10 +60,11 @@ NOTE: Password must be at least 7 characters long.''')
             #adding user preferred password to user's details
             if "Password" not in user_details.keys(): 
                 user_details["Password"] = user_password
-                database[user] = user_details               
-                
+                database[user] = user_details
+
+    #checking for new user           
     print('''
-Would you like to register another user? Respond with a "Yes" or "No" below:''')
+\nWould you like to register another user? Respond with a "Yes" or "No" below:''')
 
     new_user_query = input("Response: ").lower()
     if new_user_query == "yes":
@@ -69,10 +72,12 @@ Would you like to register another user? Respond with a "Yes" or "No" below:''')
 
     else:
         is_user_validation = False
+
+        #output user(s') details
         print(
-    '''Here are all user(s) details:''')
+'''\nHere are all user(s) details:''')
         for user, details in database.items():
-            print("\nUser:", user)
+            print("\nUser Number", user)
             for key in details:
                 print(key + ": " + details[key])
         break
